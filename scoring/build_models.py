f686c27e198092701e41e507664a8a47c5e67028
Borrman <borrmant@ummsres02.umassmed.edu>
2015-06-15 17:23:11 -0400

    added functions isolate and dG_bind

--- scoring/build_models.py
+++ scoring/build_models.py
@@ -2,6 +2,7 @@
 
 import argparse
 import pandas as pd
+import numpy as np
 import subprocess
 
 parser = argparse.ArgumentParser(description='build model complexes and score using Rosetta\'s fixbb app')
@@ -19,38 +20,98 @@ def score(pdb, weights, label):
 	Score PDB complex using Rosetta3 scoring function
 	'''
 	score_cmd = [args.ros_path + 'rosetta_source/bin/score.linuxgccrelease', '-database',
-	args.ros_path + 'rosetta_database/', '-s', args.struct_path +  pdb + '.pdb', '-out:file:scorefile', 
+	args.ros_path + 'rosetta_database/', '-s', pdb , '-out:file:scorefile', 
 	label + '_score.sc', '-extrachi_cutoff', '1', '-ex1', '-ex2', '-ex3', '-score:weights',
 	weights]
 	process = subprocess.Popen(score_cmd)
 	process.wait()
 	
+	
 def isolate(pdb, component):
 	'''
 	Isolate chain components from PDB structure
 	'''
-	if component == TCR:
-		IN = open(args.struct_path + pdb + '.pdb', 'r')
+	if component == 'TCR':
+		IN = open(pdb, 'r')
+		OUT = open(component + '.pdb', 'w')
+		for line in IN:
+			row = line.split()
+			if 'TER' not in row:
+				if row[4] == 'D' or row[4] == 'E':
+					OUT.write(line)
+		IN.close()
+		OUT.close()
+	elif component == 'pMHC':
+		IN = open(pdb, 'r')
+		OUT = open(component + '.pdb', 'w')
+		for line in IN:
+			row = line.split()
+			if 'TER' not in row:
+				if row[4] == 'A' or row[4] == 'B' or row[4] == 'C':
+					OUT.write(line)
+		IN.close()
+		OUT.close()
+	else:
+		print 'ERORR: unidentified component name'
+		quit()
 
+def dG_bind(COM_sc, TCR_sc, pMHC_sc):
+	'''
+	Calculate dG_bind scores = COM_score - (TCR_score + pMHC_score)
+	'''
+	COM = open(COM_sc, 'r')
+	TCR = open(TCR_sc, 'r')
+	pMHC= open(pMHC_sc, 'r')
 
-
-
+	header = COM.readline().split()[2:9]
+	TCR.readline()
+	pMHC.readline()
+	COM_scores = np.array(map(float, COM.readline().split()[2:9]))
+	TCR_scores = np.array(map(float, TCR.readline().split()[2:9]))
+	pMHC_scores = np.array(map(float, pMHC.readline().split()[2:9]))
+	dG_scores = COM_scores - (TCR_scores + pMHC_scores)
+	dG_hash = {}
+	for i, energy in enumerate(header):
+		dG_hash[energy] = dG_scores[i]
+	return dG_hash
 
 
 
 def main():
 	# Read Mutants table into dataframe
 	df = pd.read_csv(args.f, sep='\t')
-	counter = 0
+	# Add energy term columns
+	WF = open(args.w, 'r')
+	for line in WF:
+		energy = line.split()[0]
+		df[energy] = np.nan
+
 	for i, row in df.iterrows():
 		# Check if we need to model entry
 		if pd.notnull(row['true_PDB']):
 			true_pdb = str(row['true_PDB'])
 			# Score TCR-pMHC
-			score(true_pdb, args.w, 'com')
+			score(args.struct_path +  true_pdb + '.pdb', args.w, 'COM')
 			# Score TCR
+			isolate(args.struct_path + true_pdb + '.pdb', 'TCR')
+			score('TCR.pdb', args.w, 'TCR')
+			# Score pMHC
+			isolate(args.struct_path + true_pdb + '.pdb', 'pMHC')
+			score('pMHC.pdb', args.w, 'pMHC')
+			# Calculate dG bind
+			dG_scores = dG_bind('COM_score.sc', 'TCR_score.sc', 'pMHC_score.sc')
+			# Append to dataframe
+			for energy in dG_scores:
+				df.loc[i, energy] = dG_scores[energy]
 
-	print counter
+		# Design model TCR-pMHC
+		elif pd.isnull(row['true_PDB']):
+
+			
+
+
+
+
 
 
 
