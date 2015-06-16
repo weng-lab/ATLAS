8452bf9686f1cb1b7f0d13fc23259b49c13a1dbb
Borrman <borrmant@ummsres02.umassmed.edu>
2015-06-16 01:58:28 -0400

    add resfile function

--- scoring/build_models.py
+++ scoring/build_models.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 import pandas as pd
 import numpy as np
 import subprocess
+import re
 
 parser = argparse.ArgumentParser(description='build model complexes and score using Rosetta\'s fixbb app')
 parser.add_argument('-f', help='ATLAS Mutants tab-delimited file (ex. Mutants_052915.tsv)', type=str, dest='f',required=True)
@@ -73,8 +75,62 @@ def dG_bind(COM_sc, TCR_sc, pMHC_sc):
 	dG_hash = {}
 	for i, energy in enumerate(header):
 		dG_hash[energy] = dG_scores[i]
+	COM.close()
+	TCR.close()
+	pMHC.close()
 	return dG_hash
 
+def make_resfile(MHC_mut, MHC_mut_chain, TCR_mut, TCR_mut_chain, PEP_mut):
+	'''
+	Make resfile specifying mutations to design using fixbb app
+	NOTE:
+	Follow Brian Pierce's naming convention in the tcr_structure_database, namely:
+	MHC chain A -> chain A
+	MHC chain B -> chain B
+	TCR chain A -> chain D
+	TCR chain B -> chain E
+	peptide -> chain C
+	'''
+
+	tcr_chain_map = {'A': 'D', 'B':'E'}
+	RF = open('resfile', 'w')
+	RF.write('NATRO\nstart\n')
+	if MHC_mut != 'WT':
+		if pd.isnull(MHC_mut_chain):
+			print 'ERROR: MHC_mut_chain missing'
+			quit()
+		muts = re.findall('[A-Z]\d+[A-Z]', MHC_mut)
+		chains = re.findall('[A-Z]', MHC_mut_chain)
+		if len(muts) != len(chains):
+			print 'ERROR: not a chain for every mut'
+			quit()
+		for i in range(len(muts)):
+			search_obj = re.search('[A-Z](\d+)([A-Z])', muts[i])
+			res_num = search_obj.group(1)
+			mut_aa = search_obj.group(2)
+			RF.write(res_num + ' ' + chains[i] + ' PIKAA ' + mut_aa + '\n')
+	if TCR_mut != 'WT':
+		if pd.isnull(TCR_mut_chain):
+			print 'ERROR: TCR_mut_chain missing'
+			quit()
+		muts = re.findall('[A-Z]\d+[A-Z]', TCR_mut)
+		chains = re.findall('[A-Z]', TCR_mut_chain)
+		if len(muts) != len(chains):
+			print 'ERROR: not a chain for every mut'
+			quit()
+		for i in range(len(muts)):
+			search_obj = re.search('[A-Z](\d+)([A-Z])', muts[i])
+			res_num = search_obj.group(1)
+			mut_aa = search_obj.group(2)
+			RF.write(res_num + ' ' + tcr_chain_map[chains[i]] + ' PIKAA ' + mut_aa + '\n')
+	if PEP_mut != 'WT':
+		muts = re.findall('[A-Z]\d+[A-Z]', PEP_mut)
+		for mut in muts:
+			search_obj = re.search('[A-Z](\d+)([A-Z])', mut)
+			res_num = search_obj.group(1)
+			mut_aa = search_obj.group(2)
+			RF.write(res_num + ' C PIKAA ' + mut_aa + '\n')
+	RF.close()
 
 
 def main():
@@ -103,9 +159,26 @@ def main():
 			# Append to dataframe
 			for energy in dG_scores:
 				df.loc[i, energy] = dG_scores[energy]
+			# Remove temporary files
+			os.remove('COM_score.sc')
+			os.remove('TCR_score.sc')
+			os.remove('pMHC_score.sc')
+			os.remove('TCR.pdb')
+			os.remove('pMHC.pdb')
 
 		# Design model TCR-pMHC
 		elif pd.isnull(row['true_PDB']):
+			template_pdb = str(row['template_PDB'])
+			# Get mutations that need to be designed
+			MHC_mut = df.loc[i, 'MHC_mut']
+			MHC_mut_chain = df.loc[i, 'MHC_mut_chain']
+			TCR_mut = df.loc[i, 'TCR_mut']
+			TCR_mut_chain = df.loc[i, 'TCR_mut_chain']
+			PEP_mut = df.loc[i, 'PEP_mut']
+			# Make resfile for fixbb app
+			make_resfile(MHC_mut, MHC_mut_chain, TCR_mut, TCR_mut_chain, PEP_mut)
+			quit()
+
 
 			
 
