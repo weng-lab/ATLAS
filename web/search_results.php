c0d39cc4023a47d4179b6b4ba666ee5f2a5e5998
tborrman <tylerborrman@gmail.com>
2016-01-03 20:20:00 -0500

    Invalid search

--- web/search_results.php
+++ web/search_results.php
@@ -37,157 +37,170 @@
         </div>
         <?php  
         $link = database_connect();
-        if(isset($_POST['SEL'])) {
+        // Check if input is valid
+        if(is_numeric($_POST['dG']) && ! is_numeric($_POST['peptide'])) {
 
-            // Set query columns
-            $query_columns = array("TCRname", "TCR_mut", "TCR_mut_chain", "MHCname", "MHC_mut", "MHC_mut_chain", "PEPseq", "PEP_mut", "Kd_microM", "DeltaG_kcal_per_mol", "true_PDB", "template_PDB", "PMID"); 
-            $query_columns_str = join(', ', $query_columns);
-            $col_count = 13;
+            if(isset($_POST['SEL'])) {
+
+                // Set query columns
+                $query_columns = array("TCRname", "TCR_mut", "TCR_mut_chain", "MHCname", "MHC_mut", "MHC_mut_chain", "PEPseq", "PEP_mut", "Kd_microM", "DeltaG_kcal_per_mol", "true_PDB", "template_PDB", "PMID"); 
+                $query_columns_str = join(', ', $query_columns);
+                $col_count = 13;
 
 
-            // Get all parameters from search form
-            $search_params = array();
-            if (isset($_POST['TCR'])) {
-                if ($_POST['TCR'] != 'all') {
-                    $search_params[] = "(TCRname = '" . $_POST['TCR'] . "')";
-                }           
-            }
-            if (isset($_POST['TRAV'])) {
-                if ($_POST['TRAV'] != 'all') {                    
-                    $query = "SELECT TCRname FROM TCRs WHERE TRAV='" . $_POST['TRAV'] . "';";
-                    $TRAV_result = mysqli_query($link, $query) or die(mysqli_error($link));
-                    $i=0;
-                    while($row = mysqli_fetch_array($TRAV_result)) {
-                        $TRAVtcrs[$i] = $row['TCRname'];
-                        $i++;
-                    }
-                    $or_string = '';
-                    for ($i = 0; $i < count($TRAVtcrs); $i++) {
-                        $or_string .= "TCRname = '" . $TRAVtcrs[$i] . "'";
-                        if ($i < count($TRAVtcrs)-1) {
-                            $or_string.= " OR ";
-                        }
-                    }
-                    $search_params[] = "(" . $or_string . ")";
-                }           
-            }
-            if (isset($_POST['TRBV'])) {
-                if ($_POST['TRBV'] != 'all') {
-                    $query = "SELECT TCRname FROM TCRs WHERE TRBV='" . $_POST['TRBV'] . "';";
-                    $TRBV_result = mysqli_query($link, $query) or die(mysqli_error($link));
-                    $i=0;
-                    while($row = mysqli_fetch_array($TRBV_result)) {
-                        $TRBVtcrs[$i] = $row['TCRname'];
-                        $i++;
-                    }
-                    $or_string = '';
-                    for ($i = 0; $i < count($TRBVtcrs); $i++) {
-                        $or_string .= "TCRname = '" . $TRBVtcrs[$i] . "'";
-                        if ($i < count($TRBVtcrs)-1) {
-                            $or_string.= " OR ";
-                        }
-                    }
-                    $search_params[] = "(" . $or_string . ")";
-                }           
-            }
-
-            if (isset($_POST['MHCclass'])) {
-                if ($_POST['MHCclass'] != 'all') {
-                    $query = "SELECT MHCname FROM MHCs WHERE class='" . $_POST['MHCclass'] . "';";
-                    $MHCclass_result = mysqli_query($link, $query) or die(mysqli_error($link));
-                    $i=0;
-                    while($row = mysqli_fetch_array($MHCclass_result)) {
-                        $class_mhcs[$i] = $row['MHCname'];
-                        $i++;
-                    }
-                    $or_string = '';
-                    for ($i = 0; $i < count($class_mhcs); $i++) {
-                        $or_string .= "(MHCname LIKE '%" . $class_mhcs[$i] . 
-                                "%' OR MHCname_PDB LIKE '%" . $class_mhcs[$i] . "%')";
-                        if ($i < count($class_mhcs)-1) {
-                            $or_string.= " OR ";
-                        }
-                    }
-                    $search_params[] = "(" . $or_string . ")";
-                }           
-            }
-
-            if (isset($_POST['MHCname'])) {
-                if ($_POST['MHCname'] != 'all') {
-                    $search_params[] = "(MHCname LIKE '%" . $_POST['MHCname'] .
-                        "%' OR MHCname_PDB LIKE '%" . $_POST['MHCname'] . "%')";
+                // Get all parameters from search form
+                $search_params = array();
+                if (isset($_POST['TCR'])) {
+                    if ($_POST['TCR'] != 'all') {
+                        $search_params[] = "(TCRname = '" . $_POST['TCR'] . "')";
+                    }           
                 }
-            }
-
-            if (isset($_POST['dG'])) {
-                $search_params[] = "(DeltaG_kcal_per_mol < '" . $_POST['dG'] . "')";
-            }
-            if (isset($_POST['peptide'])) {
-                if ($_POST['peptide'] != 'all') {
-                    $search_params[] = "(PEPseq LIKE '%" . $_POST['peptide'] . "%')";
+                if (isset($_POST['TRAV'])) {
+                    if ($_POST['TRAV'] != 'all') {                    
+                        $query = "SELECT TCRname FROM TCRs WHERE TRAV='" . $_POST['TRAV'] . "';";
+                        $TRAV_result = mysqli_query($link, $query) or die(mysqli_error($link));
+                        $i=0;
+                        while($row = mysqli_fetch_array($TRAV_result)) {
+                            $TRAVtcrs[$i] = $row['TCRname'];
+                            $i++;
+                        }
+                        $or_string = '';
+                        for ($i = 0; $i < count($TRAVtcrs); $i++) {
+                            $or_string .= "TCRname = '" . $TRAVtcrs[$i] . "'";
+                            if ($i < count($TRAVtcrs)-1) {
+                                $or_string.= " OR ";
+                            }
+                        }
+                        $search_params[] = "(" . $or_string . ")";
+                    }           
                 }
-            }
+                if (isset($_POST['TRBV'])) {
+                    if ($_POST['TRBV'] != 'all') {
+                        $query = "SELECT TCRname FROM TCRs WHERE TRBV='" . $_POST['TRBV'] . "';";
+                        $TRBV_result = mysqli_query($link, $query) or die(mysqli_error($link));
+                        $i=0;
+                        while($row = mysqli_fetch_array($TRBV_result)) {
+                            $TRBVtcrs[$i] = $row['TCRname'];
+                            $i++;
+                        }
+                        $or_string = '';
+                        for ($i = 0; $i < count($TRBVtcrs); $i++) {
+                            $or_string .= "TCRname = '" . $TRBVtcrs[$i] . "'";
+                            if ($i < count($TRBVtcrs)-1) {
+                                $or_string.= " OR ";
+                            }
+                        }
+                        $search_params[] = "(" . $or_string . ")";
+                    }           
+                }
 
-            $where_query = join(' AND ', $search_params);
-            if (empty($search_params)) {
-                $query = "SELECT {$query_columns_str} FROM Mutants;";
-                
+                if (isset($_POST['MHCclass'])) {
+                    if ($_POST['MHCclass'] != 'all') {
+                        $query = "SELECT MHCname FROM MHCs WHERE class='" . $_POST['MHCclass'] . "';";
+                        $MHCclass_result = mysqli_query($link, $query) or die(mysqli_error($link));
+                        $i=0;
+                        while($row = mysqli_fetch_array($MHCclass_result)) {
+                            $class_mhcs[$i] = $row['MHCname'];
+                            $i++;
+                        }
+                        $or_string = '';
+                        for ($i = 0; $i < count($class_mhcs); $i++) {
+                            $or_string .= "(MHCname LIKE '%" . $class_mhcs[$i] . 
+                                    "%' OR MHCname_PDB LIKE '%" . $class_mhcs[$i] . "%')";
+                            if ($i < count($class_mhcs)-1) {
+                                $or_string.= " OR ";
+                            }
+                        }
+                        $search_params[] = "(" . $or_string . ")";
+                    }           
+                }
+
+                if (isset($_POST['MHCname'])) {
+                    if ($_POST['MHCname'] != 'all') {
+                        $search_params[] = "(MHCname LIKE '%" . $_POST['MHCname'] .
+                            "%' OR MHCname_PDB LIKE '%" . $_POST['MHCname'] . "%')";
+                    }
+                }
+
+                if (isset($_POST['dG'])) {
+                    $search_params[] = "(DeltaG_kcal_per_mol < '" . $_POST['dG'] . "')";
+                }
+                if (isset($_POST['peptide'])) {
+                    if ($_POST['peptide'] != 'all') {
+                        $search_params[] = "(PEPseq LIKE '%" . $_POST['peptide'] . "%')";
+                    }
+                }
+
+                $where_query = join(' AND ', $search_params);
+                if (empty($search_params)) {
+                    $query = "SELECT {$query_columns_str} FROM Mutants;";
+                    
+                }
+                else {
+                    $query = "SELECT {$query_columns_str} FROM Mutants WHERE {$where_query};";
+                }
+                $result=mysqli_query($link, $query) or die(mysqli_error($link));   
             }
-            else {
-                $query = "SELECT {$query_columns_str} FROM Mutants WHERE {$where_query};";
-            }
-            $result=mysqli_query($link, $query) or die(mysqli_error($link));   
-        }
-        ?>
-        <div class="container">
-            <table class= "table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th>TCR name</th>
-                        <th>TCR mutation</th>
-                        <th>TCR mutation chain</th>
-                        <th>MHC allele</th>
-                        <th>MHC mutation</th>
-                        <th>MHC mutation chain</th>
-                        <th>Peptide</th>
-                        <th>Peptide mutation</th>
-                        <th><span style="white-space: nowrap;">K<sub>D</sub> (&#956M)</span></th>
-                        <th><span style="white-space: nowrap;">&#916G (kcal mol<sup>-1</sup>)</span></th>
-                        <th>PDB</th>
-                        <th>Template PDB</th>
-                        <th>PMID</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    while ($row = mysqli_fetch_array($result)) {
-                        echo "<tr>";
-                        for ($i=0; $i<$col_count; $i++) {
-                            echo "<td>";
-                            if ($query_columns[$i] == "MHCname") {
-                                ?>
-                                <span style="white-space: nowrap;"> 
-                                <?php 
-                                $MHCname_arr = explode("|", $row[$query_columns[$i]]);
-                                foreach ($MHCname_arr as $allele) {
-                                    echo $allele;
-                                    ?> <br></br> <?php
+            ?>
+            <div class="container">
+                <table class= "table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>TCR name</th>
+                            <th>TCR mutation</th>
+                            <th>TCR mutation chain</th>
+                            <th>MHC allele</th>
+                            <th>MHC mutation</th>
+                            <th>MHC mutation chain</th>
+                            <th>Peptide</th>
+                            <th>Peptide mutation</th>
+                            <th><span style="white-space: nowrap;">K<sub>D</sub> (&#956M)</span></th>
+                            <th><span style="white-space: nowrap;">&#916G (kcal mol<sup>-1</sup>)</span></th>
+                            <th>PDB</th>
+                            <th>Template PDB</th>
+                            <th>PMID</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "<tr>";
+                            for ($i=0; $i<$col_count; $i++) {
+                                echo "<td>";
+                                if ($query_columns[$i] == "MHCname") {
+                                    ?>
+                                    <span style="white-space: nowrap;"> 
+                                    <?php 
+                                    $MHCname_arr = explode("|", $row[$query_columns[$i]]);
+                                    foreach ($MHCname_arr as $allele) {
+                                        echo $allele;
+                                        ?> <br></br> <?php
+                                    }
+                                    ?>
+                                    </span>
+                                    <?php
                                 }
-                                ?>
-                                </span>
-                                <?php
+                                else {
+                                    echo $row[$query_columns[$i]];
+                                }
+                                echo "</td>"; 
                             }
-                            else {
-                                echo $row[$query_columns[$i]];
-                            }
-                            echo "</td>"; 
+                            echo "</tr>";
                         }
-                        echo "</tr>";
-                    }
-                    ?>
-                </tbody>
-            </table>
-        </div>
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        <?php
+        }
+        else {
+            ?> 
+            <div class = "well">
+                <p> Invalid search selection </p>
+            </div>
+        <?php }
+        ?>
+
 
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
