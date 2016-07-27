e9ca9498dd8f9afd0e8a4815dc78be58f783e0f0
tborrman <tylerborrmant@gmail.com>
2016-07-26 23:09:52 -0400

    ddG feature

--- scoring/feature_analysis_ddG.py
+++ scoring/feature_analysis_ddG.py
@@ -8,20 +8,20 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='Run LOCOCV on 2^8 feature combinations and record correlations between predicted'+
 	' and experimentally determined affinity')
-parser.add_argument('-in', help='data table output by energy_table.py', type=str, dest='infile', required=True)
+parser.add_argument('-in', help='ddG formatted data table (energy_table_ddG.txt)', type=str, dest='infile', required=True)
 args = parser.parse_args()
 
-def plot_experiment_vs_predict(dGs, predictions, subset_features):
+def plot_experiment_vs_predict(ddGs, predictions, subset_features):
 	
 	plt.figure()
-	plt.plot(range(-15,1), range(-15,1), c='r')
-	plt.xlim(-15,-4)
-	plt.ylim(-12,-2)
-	plt.scatter(dGs, predictions)
+	plt.plot(range(-11,11), range(-11,11), c='r')
+	plt.xlim(-10,10)
+	plt.ylim(-6,6)
+	plt.scatter(ddGs, predictions)
 	plt.title('ATLAS TCR-pMHC complexes')
-	plt.xlabel('Experimentally measured binding affinity (kcal/mol)')
-	plt.ylabel('Predicted binding affinity (kcal/mol)')
-	plt.savefig('subset_plots/' + subset_features + '.png')
+	plt.xlabel('Experimentally measured ddG (kcal/mol)')
+	plt.ylabel('Predicted ddG (kcal/mol)')
+	plt.savefig('subset_plots_ddG/' + subset_features + '.png')
 	plt.close()
 	return
 
@@ -30,18 +30,20 @@ def main():
 	# Open data table
 	IN = open(args.infile, 'r')
 	# Column indices for energy terms
-	feature_cols = [37,40,41,42,43,44,45,46]
-	pdb_cols = [16, 21]
+	feature_cols = [38,41,42,43,44,45,46,47]
+	pdb_cols = [17,22]
 	num_features = len(feature_cols)
 	# Load data and normalize
-	dGs, X, header, pdbs =  lrf.load_data(IN, feature_cols, pdb_cols)
-	num_samples=len(dGs)
+	ddGs, X, header, pdbs = lrf.load_data_ddG(IN, feature_cols, pdb_cols)
+
+	num_samples=len(ddGs)
 	X = lrf.add_constant(X)
 	for i in range(1, num_features + 1):
 		X[:,i] = lrf.feature_scale(X[:,i])
 
+	
 	print '\nStatsmodel'
-	res_ols = sm.OLS(dGs, X).fit()
+	res_ols = sm.OLS(ddGs, X).fit()
 	print 'Coef:'
 	print res_ols.params
 	print res_ols.summary()
@@ -49,8 +51,8 @@ def main():
 	print res_ols.pvalues
 
 	print '\nHeader'
-	print header	
-	
+	print header
+
 	# Get powerset of 8 features
 	ps = lrf.powerset(range(1,9))
 	format_header = np.insert(header[:-2], 0,'1')
@@ -58,24 +60,24 @@ def main():
 	for subset in ps[1:]:
 		ps_header.append(format_header[subset])
 	# Perform LOCOCV on each subset of features and record correlation between predicted and experimental affinity
-	OUT = open('feature_analysis.txt', 'w')
+	OUT = open('feature_analysis_ddG.txt', 'w')
 	OUT.write('subset\tRMSE (kcal\mol)\tr\n')
 	for index, subset in enumerate(ps[1:]):
 		subset_X = X[:, [0] + subset]
 		# Leave one complex out cross-validation (LOCOCV)
 		predictions = []
 		for i in range(len(subset_X)):
-			prediction = lrf.LOCOCV(i, subset_X, dGs, pdbs)
+			prediction = lrf.LOCOCV(i, subset_X, ddGs, pdbs)
 			predictions.append(prediction)
 		predictions = np.array(predictions)
 		OUT.write(' '.join(ps_header[index]) + '\t')
 		# Stats
-		testErr = lrf.RMSE(predictions, dGs)
+		testErr = lrf.RMSE(predictions, ddGs)
 		OUT.write(str(testErr) + '\t')
-		r = np.corrcoef(predictions, dGs)
+		r = np.corrcoef(predictions, ddGs)
 		OUT.write(str(r[0,1])+'\n')
 		# Plot
-		plot_experiment_vs_predict(dGs, predictions, '_'.join(ps_header[index]))
+		plot_experiment_vs_predict(ddGs, predictions, '_'.join(ps_header[index]))
 	OUT.close()
 
 if __name__ == '__main__':
